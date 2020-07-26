c318df1e84b39122e3aeca682b6bae0fce85ec19
cavesa10 <cavesa10@gmail.com>
2020-07-26 14:20:06 -0500

    ACreando primero ajustes

---
+++ src/components/Formulario.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Formulario = () => {
+  return (
+    <form>
+      <div className="row">
+        <div className="form-group col-md-8">
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            placeholder="Busca Una imagen, ejemplo: fútbol o café"
+          />
+        </div>
+        <div className="form-group col-md-4">
+          <input
+            type="submit"
+            className="btn btn-lg btn-danger btn-block"
+            value="Buscar"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
